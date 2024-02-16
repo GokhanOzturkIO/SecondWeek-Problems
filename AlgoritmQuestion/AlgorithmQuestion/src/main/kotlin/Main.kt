08b5027cb3d85e901dbc625f73dcdce972bb3724
Halil Yılmaz <Halilylm1903@gmail.com>
2024-02-16 23:20:10 +0300

    Type Conversion Question 2 answered

--- AlgoritmQuestion/AlgorithmQuestion/src/main/kotlin/Main.kt
+++ AlgoritmQuestion/AlgorithmQuestion/src/main/kotlin/Main.kt
@@ -1,60 +1,77 @@
 fun main() {
-    println("Hello !!!")
+    do {
+        println("Hello !!!")
 
-    print("Lütfen bir sayı girin: ")
-    val number1 = readlnOrNull()
+        print("Lütfen bir sayı girin: ")
+        val number1 = readlnOrNull()
 
-    print("Lütfen matematiksel işlemin karakterini giriniz: ")
-    val operator = readlnOrNull()
+        print("Lütfen matematiksel işlemin karakterini giriniz: ")
+        val operator = readlnOrNull()
 
-    print("Lütfen ikinci sayiyi giriniz: ")
-    val number2 = readlnOrNull()
+        print("Lütfen ikinci sayiyi giriniz: ")
+        val number2 = readlnOrNull()
 
-    var result: Double = 0.0
+        var result: Double = 0.0
 
-    when(operator) {
-        "+" -> {
-            if (number1 != null && number2 != null) {
-                result = number1.toDouble() + number2.toDouble()
-                println("Sonuç = $result")
-            }else {
-                println("Lütfen sayilari eksiksiz giriniz !!!")
+        when(operator) {
+            "+" -> {
+                if (number1 != null && number2 != null) {
+                    result = number1.toDouble() + number2.toDouble()
+                    println("Sonuç = $result")
+                }else {
+                    println("Lütfen sayilari eksiksiz giriniz !!!")
+                }
+            }
+            "-" -> {
+                if (number1 != null && number2 != null) {
+                    result = number1.toDouble() - number2.toDouble()
+                    println("Sonuç = $result")
+                }else {
+                    println("Lütfen sayilari eksiksiz giriniz !!!")
+                }
+            }
+            "/" -> {
+                if (number1 != null && number2 != null) {
+                    result = number1.toDouble() / number2.toDouble()
+                    println("Sonuç = $result")
+                }else {
+                    println("Lütfen sayilari eksiksiz giriniz !!!")
+                }
+            }
+            "*" -> {
+                if (number1 != null && number2 != null) {
+                    result = number1.toDouble() * number2.toDouble()
+                    println("Sonuç = $result")
+                }else {
+                    println("Lütfen sayilari eksiksiz giriniz !!!")
+                }
+            }
+            "%" -> {
+                if (number1 != null && number2 != null) {
+                    result = number1.toDouble() % number2.toDouble()
+                    println("Sonuç = $result")
+                }else {
+                    println("Lütfen sayilari eksiksiz giriniz !!!")
+                }
+            }
+            else -> {
+                println("Hatalı operatör girdiniz !!!")
             }
         }
-        "-" -> {
-            if (number1 != null && number2 != null) {
-                result = number1.toDouble() - number2.toDouble()
-                println("Sonuç = $result")
-            }else {
-                println("Lütfen sayilari eksiksiz giriniz !!!")
+
+        print("Devam etmek istiyor musunuz? (Evet(1) / Hayır(0)): ")
+        val continueInput: String? = readlnOrNull()
+
+        if(continueInput != null) {
+            if (continueInput == "0") {
+                println("Program sonlandırılıyor...")
+                break // Döngüden çık
+            }else if(continueInput != "0" || continueInput != "1") {
+                println("Lütfen doğru bir değer giriniz !!!")
+                continue
             }
         }
-        "/" -> {
-            if (number1 != null && number2 != null) {
-                result = number1.toDouble() / number2.toDouble()
-                println("Sonuç = $result")
-            }else {
-                println("Lütfen sayilari eksiksiz giriniz !!!")
-            }
-        }
-        "*" -> {
-            if (number1 != null && number2 != null) {
-                result = number1.toDouble() * number2.toDouble()
-                println("Sonuç = $result")
-            }else {
-                println("Lütfen sayilari eksiksiz giriniz !!!")
-            }
-        }
-        "%" -> {
-            if (number1 != null && number2 != null) {
-                result = number1.toDouble() % number2.toDouble()
-                println("Sonuç = $result")
-            }else {
-                println("Lütfen sayilari eksiksiz giriniz !!!")
-            }
-        }
-        else -> {
-            println("Hatalı operatör girdiniz !!!")
-        }
-    }
+
+
+    } while (true)
 }
