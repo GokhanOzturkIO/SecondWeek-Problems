21b2583bfbbde2afd1d8f125c1fc5287e0b5fbdf
Asiman Jalilov <a.jalilov@landauschool.com>
2024-02-19 15:37:08 +0400

    sollution for algo got some progress

--- AlgoAnswer.kt
+++ AlgoAnswer.kt
@@ -1,6 +1,22 @@
+
+var userOperationIsPlus: Boolean = false
+var userOperationIsMinus : Boolean = false
+var userOperaitonIsMultiply : Boolean = false
+var userOperationIsDivision : Boolean = false
+var userOperationChecked : Boolean = false
+var numberIsInt: Boolean = false
+var numberIsFloat: Boolean = false
+var userOperation : String? = null
+var userNumberFirst = null
+var userNumberSecond = null
+var userFirtsNumberIntOrFloat = null
+var userSecondNumberIntOrFloat = null
+var calculatedValue = null
+
 fun main(){
-val userInput : String = readLine()!!
-println("$userInput")
+    val userInput : String = readLine()!!
+    println("$userInput")
+
 }
 fun welcomeWord(){
     println("Welcome to calculator")
@@ -8,17 +24,14 @@ fun welcomeWord(){
 
 fun getUserInputs(){
     println("Enter the a number:")
-    val userNumber = readLine()
+    userNumber = readLine()
     val userNumberInt = userNumber?.toIntOrNull()
     val userNumberFloat = userNumber?.toFloatOrNull()
-    var numberIsInt: Boolean = false
-    var numberIsFloat: Boolean = false
+
     val checkedNumber = checkNumberValue(userNumberInt, userNumberFloat,numberIsInt,numberIsFloat)
     if (checkedNumber){
 
-
     }
-
 }
 fun checkNumberValue(userNumberInt: Int?, userNumberFloat: Float?,
                      numberIsInt: Boolean, numberIsFloat: Boolean): Boolean{
@@ -34,4 +47,21 @@ fun checkNumberValue(userNumberInt: Int?, userNumberFloat: Float?,
         getUserInputs()
     }
     return updatedNumberIsInt || updatedNumberIsFloat
+}
+
+fun getUserOperation(){
+    println("Enter the operation: ('+','-','*','/'")
+    userOperation = readLine()
+    if(userOperationIsPlus || userOperationIsMinus || userOperationIsDivision|| userOperaitonIsMultiply) userOperationChecked == true
+
+}
+fun calculate(){
+  calculatedValue =  when(userOperation){
+        "+" -> calculatedValue = userNumberFirst + userNumberSecond
+        "-" -> calculatedValue = userNumberFirst - userNumberSecond
+        "*" -> calculatedValue = userNumberFirst * userNumberSecond
+        "/" -> calculatedValue = userNumberFirst / userNumberSecond
+     }
+
+
 }
